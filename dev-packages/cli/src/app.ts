e7d2957842fbfe3709e3af9e041936416c8a15d1
Tobias Ortmayr <tortmayr@eclipsesource.com>
2024-07-05 10:47:49 +0200

    Revert "Update changelog for 2.2.0"
    This reverts commit 40887218234f70d69d1017aa2464ee9343f7b81a.

--- dev-packages/cli/src/app.ts
+++ dev-packages/cli/src/app.ts
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
 /********************************************************************************
- * Copyright (c) 2022-2024 EclipseSource and others.
+ * Copyright (c) 2022 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
